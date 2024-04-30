0e58255e498e533a526ef7c1b9fb93d8d3cba530
Christian Trabold <christian.trabold+codes@gmail.com>
2024-05-01 01:46:32 +0200

    ci: fix linter task
    The linter is not automatically available on Github as expected but
    needs to be initialised with the dedicated step.
    
    See https://github.com/golangci/golangci-lint-action

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,12 @@
 
 set -eu
 
-# Linter is disabled at the moment (shows linting issues but does not fail the build)
-golangci-lint run .
+# Run linter if available
+if [ -x "$(command -v golangci-lint)" ]; then
+    golangci-lint run .
+else
+    echo "INFO golangci-lint not available. Skipping..."
+fi
 go test -v -race . # Run all the tests with the race detector enabled
 go fmt .
 # vet is a tool for static analysis of Go programs.
