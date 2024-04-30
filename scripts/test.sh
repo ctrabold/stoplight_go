a47f3f8f7abb1e9fd8cff0342f5adda4af5413c4
Christian Trabold <christian.trabold+codes@gmail.com>
2024-05-01 00:48:11 +0200

    ci: let linter fail the build
    In case of linter issues the linter step will now fail loudly.
    
    It was 'silenced' before to silence warnings that haven't been fixed
    yet. This fix has been implemented with 22d1610

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@
 set -eu
 
 # Linter is disabled at the moment (shows linting issues but does not fail the build)
-golangci-lint run ||true
+golangci-lint run .
 go test -v -race . # Run all the tests with the race detector enabled
 go fmt .
 # vet is a tool for static analysis of Go programs.
